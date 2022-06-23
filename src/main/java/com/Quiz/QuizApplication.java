8997e4862ce939dfae59eed3ad875087e875e574
DESKTOP-SGPRNB8\user <jbcho123@naver.com>
2022-06-23 18:48:26 +0900

    db 연동2

--- src/main/java/com/Quiz/QuizApplication.java
+++ src/main/java/com/Quiz/QuizApplication.java
@@ -1,16 +1,37 @@
 package com.Quiz;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import javax.sql.DataSource;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@MapperScan(basePackages = "com.quiz.*")  // mapper scan 추가!!! // 베이스 패키지로 수정
 @SpringBootApplication
 public class QuizApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
 	}
+	
+	@Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        
+        //(쿼리가 들어가는 경로) 문제가 있을 시 경로확인 필수
+        //resource 파일에 저장된다.
+        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+        sessionFactory.setMapperLocations(res);
+
+        return sessionFactory.getObject();
+    }
+	
 
 }
