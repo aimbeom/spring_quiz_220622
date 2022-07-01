762a902d357d0f489252e305b939acebc48921a3
DESKTOP-SGPRNB8\user <jbcho123@naver.com>
2022-07-01 18:50:18 +0900

    jstl_core(2) / fmt

--- src/main/java/com/Quiz/lesson05/Quiz01Jstl_Core.java
+++ src/main/java/com/Quiz/lesson05/Quiz01Jstl_Core.java
@@ -1,6 +1,10 @@
 package com.Quiz.lesson05;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,7 +13,8 @@ public class Quiz01Jstl_Core {
 //	http://localhost:8080/lesson05/quiz01/1
 	@RequestMapping("/lesson05/quiz01/1")
 	public String quiz01_1() {			 			
-										 
+
+		
 		return "lesson05/jstl_core";	 
 	}	 
 }	
